44f3a98e472913fe2257b7735d0b171e53c6340a
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-11-17 19:54:06 +0500

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let about = document.getElementById("about");
+let home = document.getElementById("home");
+let conatct = document.getElementById("contact");
+
+
+about.addEventListener("click", ()=>{
+    window.location.href = "http://192.168.18.128:3000/about.html";
+})
