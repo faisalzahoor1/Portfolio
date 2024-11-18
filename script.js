e90605f89c40f2ffa2ff1717c1239a84fb65068b
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-11-19 01:47:26 +0500

    new

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-
-
 let about = document.getElementById("about");
 let home = document.getElementById("home");
 let conatct = document.getElementById("contact");
 let body = document.body;
-
-
-about.addEventListener("click", () => {
-    window.location.href = "http://192.168.18.128:3000/about.html";
-    
-})
+let toggle_box = document.querySelector(".toggle_box");
+let circle = document.querySelector(".circle");
+let checkbox = document.getElementById("checkbox");
+let savedMode = '';
+let newMode = '';
 
 home.addEventListener("click", () => {
     window.location.href = "http://192.168.18.128:3000/index.html";
 })
 
-let toggle_box = document.querySelector(".toggle_box");
-let circle = document.querySelector(".circle");
-let checkbox = document.getElementById("checkbox");
+function setBackgroundColor(mode) {
+    if (mode === 'black') {
+        body.style.background = "black";
+        if (toggle_box && circle) {
+            toggle_box.style.background = "black";
+            circle.style.background = "white";
+            circle.style.left = "25px";
+        }
+    } else {
+        body.style.background = "rgb(44, 18, 69)";
+        if (toggle_box && circle) { 
+            toggle_box.style.background = "antiquewhite";
+            circle.style.background = "black";
+            circle.style.left = "0px";
+        }
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    savedMode = localStorage.getItem('colorMode'); 
+    setBackgroundColor(savedMode);
+});
 
 
 toggle_box.addEventListener("click", () => {
-    if (checkbox.checked) {
-        circle.style.left = "25px";
-        body.style.background = "black";
-        toggle_box.style.background = "black";
-        circle.style.background = "white";
-    } else {
-        circle.style.left = "0px";
-        body.style.background = "rgb(44, 18, 69)";
-        toggle_box.style.background = "antiquewhite";
-        circle.style.background = "black";
-    }
+    newMode = checkbox.checked ? 'black' : 'rgb(44, 18, 69)';
+    setBackgroundColor(newMode);
+    localStorage.setItem('colorMode', newMode);
+});
+
+about.addEventListener("click", () => {
+    window.location.href = "http://192.168.18.128:3000/about.html";
+
 })
