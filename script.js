90caaf2b6e70e362e73ac1900fb7b1df381113fa
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-11-19 18:13:15 +0500

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let about = document.getElementById("about");
 let home = document.getElementById("home");
-let conatct = document.getElementById("contact");
+let projects = document.getElementById("project");
 let body = document.body;
 let toggle_box = document.querySelector(".toggle_box");
 let circle = document.querySelector(".circle");
@@ -11,6 +11,14 @@ let newMode = '';
 home.addEventListener("click", () => {
     window.location.href = "http://192.168.18.128:3000/index.html";
 })
+about.addEventListener("click", () => {
+    window.location.href = "http://192.168.18.128:3000/about.html";
+})
+projects.addEventListener("click", () => {
+    window.location.href = "http://192.168.18.128:3000/project.html";
+})
+
+
 
 function setBackgroundColor(mode) {
     if (mode === 'black') {
@@ -22,7 +30,7 @@ function setBackgroundColor(mode) {
         }
     } else {
         body.style.background = "rgb(44, 18, 69)";
-        if (toggle_box && circle) { 
+        if (toggle_box && circle) {
             toggle_box.style.background = "antiquewhite";
             circle.style.background = "black";
             circle.style.left = "0px";
@@ -32,7 +40,7 @@ function setBackgroundColor(mode) {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    savedMode = localStorage.getItem('colorMode'); 
+    savedMode = localStorage.getItem('colorMode');
     setBackgroundColor(savedMode);
 });
 
@@ -43,7 +51,21 @@ toggle_box.addEventListener("click", () => {
     localStorage.setItem('colorMode', newMode);
 });
 
-about.addEventListener("click", () => {
-    window.location.href = "http://192.168.18.128:3000/about.html";
+const wheel = document.querySelector(".wheel");
+const nodes = document.querySelectorAll(".node");
 
-})
+nodes.forEach(node => {
+    node.addEventListener('mouseenter', () => {
+        wheel.style.animationPlayState = 'paused';
+        nodes.forEach(node => {
+            node.style.animationPlayState = 'paused';
+        });
+    });
+
+    node.addEventListener('mouseleave', () => {
+        wheel.style.animationPlayState = 'running';
+        nodes.forEach(node => {
+            node.style.animationPlayState = 'running';
+        });
+    });
+});
